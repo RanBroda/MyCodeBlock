915e48c318c151a12577bf85e4d40ce5f70d0241
Ran Broda <ran.broda@post.runi.ac.il>
2024-06-27 01:13:08 +0300

    take care of socket and design part 2+

--- client/src/components/CodeBlockPage.js
+++ client/src/components/CodeBlockPage.js
@@ -2,22 +2,26 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
+import { Editor } from '@monaco-editor/react';
+
+
+import * as PropTypes from "prop-types";
 
 const socket = io('http://localhost:3001'); // Adjust the URL as needed
-const envVar = `${process.env.REACT_APP_ENV}`;
+
+
+
 
 const CodeBlockPage = () => {
-    const { id, title } = useParams();
+    const { id } = useParams();
     // בהגדרה של סטייטים, אתה רוצה לשמור בקומפוננט מידע באופן דינאמי, הקוד בלוק יהיה הקוד עצמו, ובכל פעם שהמשתמש ירשום משהו חדש נפעיל את סט קוד בלוק. ההגדרה הראשונית של קודבלוק יהיה נאל.
     const [codeBlock, setCodeBlock] = useState(null);
     // אותו הדבר, רק עבור טעינה. הברירת מחדל זה שהוא בלודינג.
     const [loading, setLoading] = useState(true);
     const [code, setCode] = useState('');
-
-
-
+    const [ID, setID] = useState(id);
+    const IDD = id
     useEffect(() => {
-        console.log(title)
         console.log('Fetching code block for title:', id);
         fetch(`/code-block/${id}`)
             .then(response => response.json())
@@ -26,13 +30,15 @@ const CodeBlockPage = () => {
                 console.log('code:', data.code);
                 setCodeBlock(data);
                 setCode(data.code);
+                setID(data.id);
                 setLoading(false);
-                socket.emit('join-room', data.id); // Join the room based on the code block title
+                socket.emit('join-room', data._id); // Join the room based on the code block title
             })
             .catch(error => {
                 console.error('Error fetching code block:', error);
                 setLoading(false);
             });
+
         // Handle code updates from other clients
         socket.on('code-update', (newCode) => {
             setCode(newCode);
@@ -45,9 +51,10 @@ const CodeBlockPage = () => {
         };
     }, [id]);
 
+
         const handleCodeChange = (newCode) => {
-        setCode(newCode);
-        socket.emit('code-change', { room: id, code: newCode });
+            setCode(newCode);
+            socket.emit('code-change', {room: id, code: newCode});
         };
 
         if (loading) {
@@ -61,9 +68,11 @@ const CodeBlockPage = () => {
         return (
             <div>
                 <h1>{codeBlock.title}</h1>
-                <textarea
+                <Editor
+                    height="200px"
+                    language="javascript"
                     value={code}
-                    onChange={(e) => handleCodeChange(e.target.value)}
+                    onChange={(e) => handleCodeChange(e)}
                 />
             </div>
         );
