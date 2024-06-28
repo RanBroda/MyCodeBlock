f9327e7b43bc314ebbf70081788721a0cf1d4b34
Ran Broda <ran.broda@post.runi.ac.il>
2024-06-28 11:30:44 +0300

    adjust url

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -4,7 +4,7 @@ let socket;
 
 export const initSocket = () => {
     if (!socket) {
-        socket = io('http://localhost:3001'); // Adjust the URL as needed
+        socket = io('https://mycodeblock.onrender.com'); // Adjust the URL as needed
         console.log('Socket initialized');
     }
     return socket;
