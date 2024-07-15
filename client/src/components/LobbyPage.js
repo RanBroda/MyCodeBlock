fff4d0e2ce211fc9325ea146985432dd41ca7fdd
Ran Broda <ran.broda@post.runi.ac.il>
2024-07-15 14:58:21 +0300

    dfolokh

--- client/src/components/LobbyPage.js
+++ client/src/components/LobbyPage.js
@@ -7,7 +7,7 @@ const LobbyPage = () => {
     const [codeBlocks, setCodeBlocks] = useState([]);
     useEffect(() => {
         console.log('Fetching code block for title:', );
-        fetch(`/code-block`)
+        fetch(`/api/code-block`)
             .then(response => response.json())
             .then(data => {
                 setCodeBlocks(data);
